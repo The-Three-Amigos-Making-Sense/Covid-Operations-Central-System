cd1fdb79d7cad8c0ad42131feaa63b3107ba10ef
Lars <larsriel@gmail.com>
2021-05-05 17:22:48 +0200

    First commit

---
+++ src/main/java/com/thethreeamigosmakingsense/covidoperationscentralsystem/CovidOperationsCentralSystemApplication.java
@@ -0,0 +1,13 @@
+package com.thethreeamigosmakingsense.covidoperationscentralsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CovidOperationsCentralSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CovidOperationsCentralSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/thethreeamigosmakingsense/covidoperationscentralsystem/CovidOperationsCentralSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thethreeamigosmakingsense.covidoperationscentralsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CovidOperationsCentralSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
