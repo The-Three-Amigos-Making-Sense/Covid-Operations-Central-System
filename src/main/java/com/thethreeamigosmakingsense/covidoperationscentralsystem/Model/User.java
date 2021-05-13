9e697c1399c83469b7dc02478a286f787c32eb60
periklis <per.tso93@gmail.com>
2021-05-13 15:46:45 +0200

    user class fullfilled

--- src/main/java/com/thethreeamigosmakingsense/covidoperationscentralsystem/Model/User.java
+++ src/main/java/com/thethreeamigosmakingsense/covidoperationscentralsystem/Model/User.java
@@ -1,22 +1,66 @@
 package com.thethreeamigosmakingsense.covidoperationscentralsystem.Model;
 
 public class User {
-
-    private String username;
+    private int cpr;
+    private String email;
+    private String firstName;
+    private String lastName;
+    private int phoneNo;
     private String password;
     private boolean enabled = true;
 
-    public User(String username, String password) {
-        this.username = username;
+    public User(int cpr, String email, String firstName, String lastName, int phoneNo, String password) {
+        this.cpr = cpr;
+        this.email = email;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phoneNo = phoneNo;
+        this.password = password;
+    }
+    public User(String firstName, String password) {
+        this.firstName = firstName;
         this.password = password;
     }
 
-    public String getUsername() {
-        return username;
+    public int getCpr() {
+        return cpr;
     }
 
-    public void setUsername(String username) {
-        this.username = username;
+    public void setCpr(int cpr) {
+        this.cpr = cpr;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getPhoneNo() {
+        return phoneNo;
+    }
+
+    public void setPhoneNo(int phoneNo) {
+        this.phoneNo = phoneNo;
+    }
+
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
     }
 
     public String getPassword() {
