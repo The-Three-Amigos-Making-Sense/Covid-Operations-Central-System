d07e2585f330ae0056eb6bcca3018b974bd30f6b
Lars <larsriel@gmail.com>
2021-05-14 15:13:58 +0200

    added BookingController and booking.html

---
+++ src/main/java/com/thethreeamigosmakingsense/covidoperationscentralsystem/Controller/BookingController.java
@@ -0,0 +1,13 @@
+package com.thethreeamigosmakingsense.covidoperationscentralsystem.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class BookingController {
+
+    @GetMapping("/booking")
+    public String booking() {
+        return "booking/booking";
+    }
+}
